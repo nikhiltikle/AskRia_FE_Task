7af61bead5c4aeb2289c6a3d1e503cab50c36d15
Nikhil Tikle <nikhiltikle31@gmail.com>
2023-10-12 20:05:12 +0530

    WIP card

---
+++ src/comp/Card/index.tsx
@@ -0,0 +1,22 @@
+import './index.css';
+
+import { Card as AntdCard } from 'antd';
+import React from 'react';
+
+interface CardProps {
+  title: string;
+  children: React.ReactNode;
+}
+
+const Card: React.FC<CardProps> = ({ title, children }) => {
+  return (
+    <AntdCard
+      id='form-card'
+      title={title}
+    >
+      {children}
+    </AntdCard>
+  );
+};
+
+export default Card;
