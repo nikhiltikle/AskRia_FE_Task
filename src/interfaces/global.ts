8cd37236803f7da720add04f0f967b9d8ac375fa
Nikhil Tikle <nikhiltikle31@gmail.com>
2023-10-12 20:08:40 +0530

    WIP update interface

--- src/interfaces/global.ts
+++ src/interfaces/global.ts
@@ -1,3 +1,32 @@
 import { MenuProps } from 'antd';
 
 export type MenuItem = Required<MenuProps>['items'][number];
+
+export enum QuestionType {
+  Default = '',
+  Paragraph = 'paragraph',
+  MultipleChoice = 'multipleChoice',
+  Dropdown = 'dropdown',
+  YesOrNo = 'yesOrNo',
+  Video = 'video',
+}
+
+export enum VideoDurationType {
+  Minute = 'Minute',
+  Seconds = 'Seconds',
+}
+
+export interface Question {
+  id: string;
+  type: QuestionType;
+  question: string;
+  choices: string[];
+  maxChoice: number;
+  disqualify: boolean;
+  other: boolean;
+  additionalInfo: {
+    question: string;
+    videoMaxDuration: number;
+    videoDurationType: VideoDurationType;
+  };
+}
