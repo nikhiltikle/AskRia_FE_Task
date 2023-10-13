4a07fb9a96d9bf234134d84725ba68fdbf88e632
Nikhil Tikle <nikhiltikle31@gmail.com>
2023-10-13 17:50:00 +0530

    Integrate mock API to fetch application form data

--- src/components/ApplicationForm/index.tsx
+++ src/components/ApplicationForm/index.tsx
@@ -4,13 +4,46 @@ import { Flex } from 'antd';
 import ImageUpload from '../ImageUpload';
 import PersonalInformation from '../PersonalInformation';
 import ProfileInformation from '../ProfileInformation';
+import { useApplicationFormContext } from '../../context/applicationForm';
+import { useEffect } from 'react';
+import { ApplicationFormState } from '../../interfaces/applicationForm';
 
 interface FormProps {}
 
 const ApplicationForm: React.FC<FormProps> = () => {
+  const { setFormData, coverImage, personalInformation } =
+    useApplicationFormContext();
+  console.log({ coverImage, personalInformation });
+
+  const fetchApplicationFormData = async () => {
+    try {
+      const formDataRes = await fetch(
+        'http://127.0.0.1:4010/api/398.9189931590182/programs/consequuntur/application-form'
+      );
+      const parsedFormData: ApplicationFormState = await (
+        await formDataRes.json()
+      ).data?.attributes;
+
+      setFormData(parsedFormData);
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  useEffect(() => {
+    fetchApplicationFormData();
+  }, []);
+
   return (
-    <Flex vertical gap='65px' className='application-form-container'>
-      <ImageUpload label='Upload cover image' />
+    <Flex
+      vertical
+      gap='65px'
+      className='application-form-container'
+    >
+      <ImageUpload
+        label='Upload cover image'
+        value={coverImage}
+      />
 
       <PersonalInformation />
 
