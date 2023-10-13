58c1b6faece35e74db9d069c899c18955984765f
Nikhil Tikle <nikhiltikle31@gmail.com>
2023-10-13 17:50:30 +0530

    Update list item action buttons

--- src/components/ApplicationForm/ListItemActionButton.tsx
+++ src/components/ApplicationForm/ListItemActionButton.tsx
@@ -1,17 +1,24 @@
 import { Checkbox, Flex, Switch, Typography } from 'antd';
+import { CheckboxChangeEvent } from 'antd/es/checkbox';
+import { MouseEvent } from 'react';
 
 interface ListItemActionButtonProps {
   checked?: boolean;
   internal?: boolean;
-  hide?: boolean;
-  onCheckboxChange?: () => void;
+  show?: boolean;
+  onCheckboxChange: (event: CheckboxChangeEvent) => void;
+  onSwitchChange: (
+    checked: boolean,
+    event: MouseEvent<HTMLButtonElement>
+  ) => void;
 }
 
 const ListItemActionButton: React.FC<ListItemActionButtonProps> = ({
   checked,
-  hide,
+  show,
   internal = true,
   onCheckboxChange,
+  onSwitchChange,
 }) => {
   return (
     <Flex
@@ -30,8 +37,11 @@ const ListItemActionButton: React.FC<ListItemActionButtonProps> = ({
         align='center'
         gap='16px'
       >
-        <Switch checked={!hide} />
-        <Typography>Hide</Typography>
+        <Switch
+          checked={show}
+          onChange={onSwitchChange}
+        />
+        <Typography>{show ? 'Hide' : 'Show'}</Typography>
       </Flex>
     </Flex>
   );
