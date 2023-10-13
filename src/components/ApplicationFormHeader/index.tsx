82970db7e1cde24793f5fce917c07166bb7d68cf
Nikhil Tikle <nikhiltikle31@gmail.com>
2023-10-13 16:15:14 +0530

    Add form stepper

---
+++ src/components/ApplicationFormHeader/index.tsx
@@ -0,0 +1,45 @@
+import './index.css';
+
+import { Steps } from 'antd';
+
+const StepperContent = ({ title }: { title: string }) => {
+  return (
+    <div
+      className={
+        title === 'Application Form'
+          ? 'appliaction-form-items selected triangle-right'
+          : 'appliaction-form-items'
+      }
+    >
+      {title}
+    </div>
+  );
+};
+
+const ApplicationFormHeaderItems = [
+  {
+    title: <StepperContent title={'Program Details'} />,
+    disabled: true,
+    icon: <></>,
+  },
+  {
+    title: <StepperContent title={'Application Form'} />,
+    icon: <></>,
+  },
+  {
+    title: <StepperContent title={'Workflow'} />,
+    icon: <></>,
+    disabled: true,
+  },
+  {
+    title: <StepperContent title={'Preview'} />,
+    icon: <></>,
+    disabled: true,
+  },
+];
+
+const ApplicationFormHeader = () => {
+  return <Steps items={ApplicationFormHeaderItems} />;
+};
+
+export default ApplicationFormHeader;
